7cf650f9f332b6a35c430cce2ba2f8a30c08760e
ztftrue <ztftrue@gmail.com>
2019-07-11 23:25:22 +0800

    change app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatTabsModule, MatSelectModule } from '@angular/material';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { DialogDetailsComponent } from './dialog/dialog.component';
 import { MouseWheelDirective } from './mouse-wheel.directive';
+import { EditContentComponent } from './edit-content/edit-content.component';
+import { ShowContentComponent } from './show-content/show-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DialogDetailsComponent,
-    MouseWheelDirective
+    MouseWheelDirective,
+    EditContentComponent,
+    ShowContentComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +30,10 @@ import { MouseWheelDirective } from './mouse-wheel.directive';
     MatCheckboxModule,
     MatDialogModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatTabsModule,
+    ReactiveFormsModule,
+    MatSelectModule
   ],
   providers: [],
   entryComponents: [
