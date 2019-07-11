5e8ec1368ecfaac206fe9621d1950b3b4b33a291
ztftrue <ztftrue@gmail.com>
2019-07-12 00:21:01 +0800

    add css and textare line

--- src/app/edit-content/edit-content.component.ts
+++ src/app/edit-content/edit-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewChild, NgZone } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, NgZone, AfterViewInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { CommunicationService } from '../communication.service';
 import { CdkTextareaAutosize } from '@angular/cdk/text-field';
@@ -9,8 +9,10 @@ import { take } from 'rxjs/operators';
   templateUrl: './edit-content.component.html',
   styleUrls: ['./edit-content.component.css']
 })
-export class EditContentComponent implements OnInit {
+export class EditContentComponent implements OnInit, AfterViewInit {
   @ViewChild('autosize', { static: false }) autosize: CdkTextareaAutosize;
+  @ViewChild('fontSize', { static: false }) fontSize: any;
+  textLine = 10;
   constructor(private fb: FormBuilder, private communication: CommunicationService, private ngZone: NgZone) { }
   textFormControl = new FormControl('', []);
   ngOnInit() {
@@ -18,6 +20,16 @@ export class EditContentComponent implements OnInit {
     this.communication.previewToEditObserve.subscribe((res: string) => {
       this.textFormControl.setValue(res);
     });
+   
+  }
+  ngAfterViewInit() {
+    window.onresize = ((event) => {
+      this.textLine = (screen.availHeight - 340) / parseInt(this.fontSize.value.replace('px', ''));
+      console.log(this.textLine);
+    });
+    const a = this.fontSize.value.replace('px', '');
+    this.textLine = Math.floor((screen.availHeight - 340) / parseInt(a));
+    console.log(Math.floor(this.textLine));
   }
   submitContent(values: any) {
     console.log(values);
@@ -28,6 +40,6 @@ export class EditContentComponent implements OnInit {
       .subscribe(() => this.autosize.resizeToFitContent(true));
   }
   textareaChange() {
-        this.communication.setContentToPreview(this.textFormControl.value);
+    this.communication.setContentToPreview(this.textFormControl.value);
   }
 }
