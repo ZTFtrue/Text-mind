85712089fcdf9930be0ab25478648dc90b524c10
ztftrue <ztftrue@gmail.com>
2019-07-28 02:37:18 +0800

    修复便以后不能显示公式的问题

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { ConfigService } from './config.service';
   styleUrls: ['./app.component.css'],
   encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent implements AfterViewInit, OnInit {
+export class AppComponent {
 
   selected = new FormControl(0);
   lastSelect = 0;
@@ -19,12 +19,6 @@ export class AppComponent implements AfterViewInit, OnInit {
 
   }
 
-  ngAfterViewInit(): void {
-  }
-  ngOnInit(): void {
-    this.mathJaxObject = this.cs.nativeGlobal().MathJax;
-    this.loadMathConfig();
-  }
   tabSelectedIndexChanage(event: any) {
     if (this.lastSelect !== this.selected.value) {
       this.lastSelect = this.selected.value;
@@ -33,53 +27,5 @@ export class AppComponent implements AfterViewInit, OnInit {
         this.communication.setRefreshPreview(true);
       }
     }
-  }
-  loadMathConfig() {
-    this.mathJaxObject.Hub.Config({
-      showMathMenu: false,
-      tex2jax: {
-        inlineMath: [
-          ['$', '$']
-        ],
-        displayMath: [
-          ['$$', '$$']
-        ]
-      },
-      CommonHTML: {
-        linebreaks: {
-          automatic: true
-        }
-      },
-      'HTML-CSS': {
-        linebreaks: {
-          automatic: true
-        },
-      },
-      SVG: {
-        linebreaks: {
-          automatic: true
-        },
-        mtextFontInherit: true,
-        blacker: 1,
-      },
-      // extensions: ['tex2jax.js', 'TeX/AMSmath.js'],
-      // jax: ['input/TeX', 'output/SVG'],
-      jax: ['input/MathML', 'output/SVG'],
-      extensions: ['mml2jax.js', 'MathEvents.js'],
-      MathML: {
-        extensions: ['content-mathml.js']
-      },
-      menuSettings: {
-        zoom: 'None'
-      },
-      MatchWebFonts: {
-        matchFor: {
-          SVG: true
-        },
-        fontCheckDelay: 500,
-        fontCheckTimeout: 15 * 1000
-      },
-      messageStyle: 'none'
-    });
   }
 }
--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Inject, NgZone, SecurityContext, ElementRef, ViewChild, Renderer2, Renderer } from '@angular/core';
+import { AfterViewInit, Component, Inject, NgZone, SecurityContext, ElementRef, ViewChild, Renderer2, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ConfigService } from '../config.service';
 import { DomSanitizer } from '@angular/platform-browser';
@@ -9,7 +9,8 @@ import highlight from 'highlight.js';
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.css']
 })
-export class DialogDetailsComponent implements AfterViewInit {
+export class DialogDetailsComponent implements AfterViewInit, OnInit {
+
   dataContent;
   mathJaxObject;
   dialogTitle: string;
@@ -24,14 +25,17 @@ export class DialogDetailsComponent implements AfterViewInit {
     this.dialogTitle = datatArrary.pop();
     this.dataContent = this.renderMd(datatArrary.reverse().join('\n\n'));
   }
-
+  ngOnInit(): void {
+    this.renderer2.setProperty(this.mathContent.nativeElement, 'innerHTML', this.dataContent);
+  }
   ngAfterViewInit() {
     this.mathJaxObject = this.cs.nativeGlobal().MathJax;
-    this.renderer2.setProperty(this.mathContent.nativeElement, 'innerHTML', this.dataContent);
+    this.loadMathConfig();
     this.renderMath();
   }
 
   renderMath() {
+    console.dir(this.mathJaxObject);
     this.mathJaxObject.Hub.Queue(['setRenderer', this.mathJaxObject.Hub, 'CommonHTML'],
       ['Typeset', this.mathJaxObject.Hub, this.mathContent.nativeElement], () => {
         this.detector.run(() => {
@@ -68,5 +72,53 @@ export class DialogDetailsComponent implements AfterViewInit {
       xhtml: false
     });
     return this.sanitizer.sanitize(SecurityContext.HTML, marked(content));
+  }
+  loadMathConfig() {
+    this.mathJaxObject.Hub.Config({
+      showMathMenu: false,
+      tex2jax: {
+        inlineMath: [
+          ['$', '$']
+        ],
+        displayMath: [
+          ['$$', '$$']
+        ]
+      },
+      CommonHTML: {
+        linebreaks: {
+          automatic: true
+        }
+      },
+      'HTML-CSS': {
+        linebreaks: {
+          automatic: true
+        },
+      },
+      SVG: {
+        linebreaks: {
+          automatic: true
+        },
+        mtextFontInherit: true,
+        blacker: 1,
+      },
+      // extensions: ['tex2jax.js', 'TeX/AMSmath.js'],
+      // jax: ['input/TeX', 'output/SVG'],
+      jax: ['input/MathML', 'output/SVG'],
+      extensions: ['mml2jax.js', 'MathEvents.js'],
+      MathML: {
+        extensions: ['content-mathml.js']
+      },
+      menuSettings: {
+        zoom: 'None'
+      },
+      MatchWebFonts: {
+        matchFor: {
+          SVG: true
+        },
+        fontCheckDelay: 500,
+        fontCheckTimeout: 15 * 1000
+      },
+      messageStyle: 'none'
+    });
   }
 }
