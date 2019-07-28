d5d8e30cf4f7241b9b12b0c75d0a805964744284
ztftrue <ztftrue@gmail.com>
2019-07-28 13:19:53 +0800

    open extern link

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -4,6 +4,8 @@ import { ConfigService } from '../config.service';
 import { DomSanitizer } from '@angular/platform-browser';
 import marked from 'marked';
 import highlight from 'highlight.js';
+import { shell } from 'electron';
+
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
@@ -68,6 +70,7 @@ export class DialogDetailsComponent implements AfterViewInit, OnInit {
     this.mathJaxObject.Hub.Queue(['setRenderer', this.mathJaxObject.Hub, 'CommonHTML'],
       ['Typeset', this.mathJaxObject.Hub, this.mathContent.nativeElement], () => {
         this.detector.run(() => {
+          this.externLink();
         });
       });
     // this.mathJaxObject.Hub.Queue(['setRenderer', this.mathJaxObject.Hub, 'SVG'],
@@ -78,6 +81,19 @@ export class DialogDetailsComponent implements AfterViewInit, OnInit {
 
   closeDialog() {
     this.detector.run(() => { this.dialogRef.close(); });
+  }
+
+  externLink() {
+    const links = this.mathContent.nativeElement.querySelectorAll('a[href]');
+    Array.prototype.forEach.call(links, (link: any) => {
+      const url = link.getAttribute('href');
+      if (url.indexOf('http') === 0) {
+        link.addEventListener('click', (e) => {
+          e.preventDefault();
+          shell.openExternal(url);
+        });
+      }
+    });
   }
 
   renderMd(content: string) {
